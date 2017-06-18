a7da76a063d2a32fb08632cf182d5d3fad89f6a8
vadim <zavertal.v@gmail.com>
2017-06-18 15:02:58 +0300

    the project work

--- src/main/java/com/dao/EntityDao.java
+++ src/main/java/com/dao/EntityDao.java
@@ -1,7 +1,7 @@
 package com.dao;
 
 import com.entity.Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EntityDao extends JpaRepository<Entity,Integer> {
+public interface EntityDao extends MongoRepository<Entity,String> {
 }
--- src/main/java/com/entity/Entity.java
+++ src/main/java/com/entity/Entity.java
@@ -1,12 +1,54 @@
 package com.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@javax.persistence.Entity
+@Document(collection="entity")
 public class Entity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @org.springframework.data.annotation.Id
+    private
+    String id;
+
+    private
+    String first_name;
+
+    private
+    String last_name;
+
+    private
+    String username;
+
+    public Entity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
