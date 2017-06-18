54b038b4d8710743e60e08227604581842e30833
vadim <zavertal.v@gmail.com>
2017-06-18 14:21:11 +0300

    add mongo connect to mongo db

--- src/main/java/com/ClientMongoApplication.java
+++ src/main/java/com/ClientMongoApplication.java
@@ -1,11 +1,19 @@
 package com;
 
+import com.service.EntityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientMongoApplication implements CommandLineRunner {
+	private final EntityService entityService;
+
+	@Autowired
+	public ClientMongoApplication(EntityService entityService) {
+		this.entityService = entityService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientMongoApplication.class, args);
@@ -13,6 +21,7 @@ public class ClientMongoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
+		entityService.getAllEntity().forEach(System.out::print);
 		System.out.println("HW!!!");
 	}
 }
