ed32e24501f8ff2161018d445face4a5db6e5e32
vadim <zavertal.v@gmail.com>
2017-06-26 03:05:18 +0300

    add switch and Perpetual cycle for input

--- src/main/java/com/ClientMongoApplication.java
+++ src/main/java/com/ClientMongoApplication.java
@@ -6,23 +6,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
 public class ClientMongoApplication implements CommandLineRunner {
-	private final EntityService entityService;
+    private final EntityService entityService;
 
-	@Autowired
-	public ClientMongoApplication(EntityService entityService) {
-		this.entityService = entityService;
-	}
+    @Autowired
+    public ClientMongoApplication(EntityService entityService) {
+        this.entityService = entityService;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientMongoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientMongoApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-//		entityService.getAllEntity().forEach(System.out::print);
-		System.out.println(entityService.getAllEntity());
-		System.out.println("HW!!!");
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while (true) {
+            System.out.print("write = ");
+            s = bufferedReader.readLine();
+            switch (s) {
+                case "stop":
+                    return;
+                case "view":
+                    System.out.println(entityService.getAllEntity());
+                    break;
+                default:
+                    System.out.println("write correct keyword");
+            }
+        }
+    }
 }
--- src/main/java/com/service/EntityService.java
+++ src/main/java/com/service/EntityService.java
@@ -17,16 +17,24 @@ public class EntityService {
     }
 
     public List<Entity> getAllEntity() {
-        if (entityDao.findAll().isEmpty()){
+        if (entityDao.findAll().isEmpty()) {
             test();
         }
         return entityDao.findAll();
     }
 
     private void test() {
-        Entity entity= new Entity();
+        Entity entity = new Entity();
         entity.setName("lol");
         entity.setAge(10);
         entityDao.save(entity);
+        Entity entity2 = new Entity();
+        entity2.setName("lol1");
+        entity2.setAge(102);
+        entityDao.save(entity2);
+        Entity entity3 = new Entity();
+        entity3.setName("lol2");
+        entity3.setAge(103);
+        entityDao.save(entity3);
     }
 }
