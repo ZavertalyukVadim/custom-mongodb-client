dde3f6297602fb423836287e425774fcd740012c
vadim <zavertal.v@gmail.com>
2017-07-06 05:14:20 +0300

    add dto's

---
+++ src/main/java/com/dto/AgeDto.java
@@ -0,0 +1,21 @@
+package com.dto;
+
+
+public class AgeDto {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "AgeDto{" +
+                "age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/dto/NameDto.java
@@ -0,0 +1,21 @@
+package com.dto;
+
+
+public class NameDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "NameDto{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
