586858914a3309c7474d77a8e962ff592b42e536
vadim <zavertal.v@gmail.com>
2017-07-12 03:42:06 +0300

    i do not know why this ..... tests does not runs

--- src/main/java/com/config/ApplicationConfiguration.java
+++ src/main/java/com/config/ApplicationConfiguration.java
@@ -4,4 +4,5 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
 public class ApplicationConfiguration {
+
 }
---
+++ src/test/java/com/ExampleDataMongoNonEmbeddedTests.java
@@ -0,0 +1,25 @@
+package com;
+
+import com.dao.EntityDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+public class ExampleDataMongoNonEmbeddedTests {
+ @Autowired
+    private EntityDao entityDao;
+
+    @Test
+    public void testExample() throws Exception {
+
+        assertThat(null,is(entityDao.findAll()));
+    }
+}
