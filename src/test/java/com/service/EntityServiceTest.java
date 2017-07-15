05ee4aad0deadfbf8be31d92f7e4ee6fb028bf67
vadim <zavertal.v@gmail.com>
2017-07-15 20:37:12 +0300

    add first test on entityServiceTest

--- src/test/java/com/service/EntityServiceTest.java
+++ src/test/java/com/service/EntityServiceTest.java
@@ -1,10 +1,21 @@
 package com.service;
 
+import com.dao.ObjectDao;
+import com.dto.EntityDto;
+import com.dto.SqlDto;
+import com.parser.Parser;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
 
 
 @RunWith(SpringRunner.class)
@@ -12,5 +23,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class EntityServiceTest {
     @Autowired
     private EntityService entityService;
+    @Autowired
+    private ObjectDao objectDao;
+
+    @Test
+    public void testGetAllFieldsFromEntity(){
+        SqlDto query;
+        Parser parser =  new Parser();
+        String exampleSql = "SELECT * FROM entity WHERE name = `lol` GROUP BY name, sex, object ORDER BY name ASC LIMIT 3 OFFSET 0";
+        List<EntityDto> entity = new ArrayList<>();
+        EntityDto entityDto= new EntityDto();
+        entityDto.setName("lol");
+        entityDto.setSex("M");
+        entityDto.setObject(objectDao.getByLastName("last"));
+        entity.add(entityDto);
+
+        query = parser.parse(exampleSql);
+
+        Assert.assertThat(entity, is(entityService.getAllFieldsFromEntity(query)));
+
+    }
 
 }
